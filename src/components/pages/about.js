f03a341d87f38eace33706ee83472cfdf28c190a
ting682 <tchung682@gmail.com>
2021-01-07 20:30:13 -0500

    added about pic

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -6,7 +6,7 @@ export const About = (props) => {
 
     return (
         <React.Fragment>
-            <img src="images/about_1.jpg" style={{width: "100%", maxWidth: "600px", paddingBottom: "30px"}}>
+            <img src="images/about_3.jpg" style={{width: "100%", maxWidth: "600px", paddingBottom: "30px"}}>
                 
             </img>
            
